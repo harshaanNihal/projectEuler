54ccda563a464ff3537c085767457765abde6b4f
klassynihal <harshaan.n.khan@gmail.com>
2018-10-31 11:02:26 +0530

    eleven.js

---
+++ 16.js
@@ -0,0 +1,22 @@
+// 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+// What is the sum of the digits of the number 21000 ?
+
+function problem(a,b) {
+  var res = Number(Math.pow(a,b));
+  sum = 0;
+  while (res % 10 == 0) {
+    res = res / 10;
+  }
+  while(res > 0) {
+    if (res % 10 == 0) {
+      res = res / 10;
+    }
+    else {
+      let lDigit = (res % 10);
+      res = Math.floor(res / 10);
+      sum += lDigit;
+      console.log(sum );
+    }
+  }
+}
+problem(2,1000);
